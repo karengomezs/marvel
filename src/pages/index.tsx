3ae3c18c18e9d0c42a7e1078a90ba190b7d48510
karengomezs <karengomezs0601@gmail.com>
2023-05-11 19:23:53 -0500

    card with vectors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,38 +2,42 @@ export default function Home() {
   return (
     <main className="px-16">
       {/* div primera seccion */}
-      <div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
 
       {/* div segunda sección */}
 
-      <div className="relative w-60 h-[448px] text-gold-1 flex flex-col justify-center items-center gap-6 rounded-lg ">
-        <img src="/cardbg.png " className="absolute z-[-1]" alt="" />
-        <img src="/vectores.png" className="absolute top-28" alt="" />
+      <div className="relative w-fit mt-20">
+        <img src="/cardbg.png " className="" alt="" />
 
-        <div className="flex items-center">
-          <img className="h-6" src="/vector1.png" alt="" />
-          <p className="text-center mx-3">
-            PERSONAJE <br /> NAME
-          </p>
-          <img className="h-6" src="/vector2.png" alt="" />
-        </div>
-
-        <img className="top-20 h-32  w-32" src="/mask-group.png" alt="" />
-
-        <div className="flex flex-col gap-5">
-          <div className="w-44 h-12 rounded-lg border-[0.75px] border-gris-3 bg-gris-4 flex items-center">
-            <p className="ms-4 ">
-              Comics: <span className="ms-4">25</span>
+        <div className="absolute z-1 w-full h-full p-8 flex flex-col items-center justify-between top-0">
+          <div className="flex justify-center items-center text-gold-1 ">
+            <img className="h-6" src="/vector1.png" alt="" />
+            <p className="text-center mx-3">
+              PERSONAJE <br /> NAME
             </p>
+            <img className="h-6" src="/vector2.png" alt="" />
           </div>
-          <div className="w-44 h-12 rounded-lg border-[0.75px] border-gris-3 bg-gris-4 flex items-center">
-            <p className="ms-4 ">
-              Comics: <span className="ms-4">25</span>
-            </p>
+
+          <div className="relative">
+            <img className="top-20 h-32 w-32" src="/mask-group.png" alt="" />
+            <img
+              src="/vectores.png"
+              className="absolute top-0 scale-125"
+              alt=""
+            />
+            {/* <img src="/diamonds.png" className="absolute top-0 z-1" alt="" /> */}
+          </div>
+
+          <div className="flex flex-col gap-5 text-gold-1">
+            <div className="w-44 h-12 rounded-lg border-[0.75px] border-gris-3 bg-gris-4 flex items-center">
+              <p className="ms-4 ">
+                Comics: <span className="ms-4">25</span>
+              </p>
+            </div>
+            <div className="w-44 h-12 rounded-lg border-[0.75px] border-gris-3 bg-gris-4 flex items-center">
+              <p className="ms-4 ">
+                Comics: <span className="ms-4">25</span>
+              </p>
+            </div>
           </div>
         </div>
       </div>
