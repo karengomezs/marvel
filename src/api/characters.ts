73e4b32bad108d8661ae61a02268c6385aaa737e
karengomezs <karengomezs0601@gmail.com>
2023-05-12 15:19:39 -0500

    changed limit

--- src/api/characters.ts
+++ src/api/characters.ts
@@ -66,7 +66,7 @@ interface Thumbnail {
 }
 
 const urlBase =
-  "http://gateway.marvel.com/v1/public/characters?apikey=7e68f217f38c3e340c4abaa74c28ba0a&hash=16d0e344d494e4430bdefb30fd94e12c&ts=1&limit=10&offset=";
+  "http://gateway.marvel.com/v1/public/characters?apikey=7e68f217f38c3e340c4abaa74c28ba0a&hash=16d0e344d494e4430bdefb30fd94e12c&ts=1&limit=12&offset=";
 
 export async function getCharacters(offset = 0) {
   try {
