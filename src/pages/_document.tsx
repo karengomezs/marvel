e26c9a95b9edb77955b1704572f5eacde2a95b44
karengomezs <karengomezs0601@gmail.com>
2023-05-12 16:15:44 -0500

    progress bar letters

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,18 @@ import Nav from "@/components/nav";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head></Head>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Nav />
         <Main />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,17 +34,25 @@ export default function Home(props: PropsT) {
     <main className="px-16 pt-20 pb-16 bg-gris-4">
       <div className="flex gap-5 text-gold-1 h-48">
         <div className="border-[0.75px] border-gold-2 flex-1 rounded-md">
-          <p className="text-center mt-10">PROGRESO DE PELÍCULAS PRODUCIDAS</p>
-          <div className="border-2 border-gold-2 w-[80%] h-8 mx-auto mt-10 p-1">
+          <p className="text-center mt-7 mb-3">
+            PROGRESO DE PELÍCULAS PRODUCIDAS
+          </p>
+          <p className="font-regular text-xs text-gold-2 pe-[10%] text-right">
+            Production Goals: <br /> 100 movies
+          </p>
+          <div className="border-2 border-gold-2 w-[80%] h-8 mx-auto p-1 m-2">
             <div className="bg-gold-1 m-auto w-full h-full">
               <div className="progress-bar w-[60%] h-full"></div>
             </div>
           </div>
+          <p className="font-regular text-xs blue-font pe-[43%] text-right">
+            Produced Movies: <br /> 60 Movies
+          </p>
         </div>
         <div className="border-[0.75px] border-gold-2 w-80 rounded-sm">
           <Youtube />
         </div>
-        <div className="border-[0.75px] border-gold-2 w-80 rounded-lg">
+        <div className="border-[0.75px] border-gold-2 w-80 rounded-sm">
           <img
             className="h-full object-cover"
             src="https://i0.wp.com/imgs.hipertextual.com/wp-content/uploads/2018/12/infinity-war.jpg?fit=1000%2C563&quality=50&strip=all&ssl=1"
