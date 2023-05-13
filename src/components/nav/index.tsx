872e827944d5838c05859200ce3c36a262187e01
karengomezs <karengomezs0601@gmail.com>
2023-05-12 19:13:32 -0500

    nav ok

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -7,7 +7,7 @@ export default function Nav() {
     <nav className="bg-dark-1 text-gold-1 h-[116px] flex px-16 border-b-1 border-gris-3">
       <img src="/logo.svg" alt="" />
       <ul className="flex ">
-        <li className="w-[114px] flex items-center justify-center hover-nav group">
+        <li className="w-[114px] flex items-center justify-center group">
           <img
             className="absolute bottom-0 hidden group-hover:block"
             src="/vector.png"
@@ -15,7 +15,8 @@ export default function Nav() {
           />
           <span className="group-hover:text-gold-2">HOME</span>
         </li>
-        <li className="w-[114px] flex items-center justify-center hover-nav group relative">
+
+        <li className="w-[114px] flex items-center justify-center hover-nav group">
           <img
             className="absolute bottom-0 hidden group-hover:block"
             src="/vector.png"
@@ -23,6 +24,11 @@ export default function Nav() {
           />
           <span className="group-hover:text-gold-2">CHARACTERS</span>
         </li>
+
+        {/* <li className="w-[114px] flex items-center justify-center group relative">
+          <img className="absolute bottom-0 hidden" src="/vector.png" alt="" />
+          <span className="group-hover:text-gold-2">CHARACTERS</span>
+        </li> */}
       </ul>
 
       <div className="flex gap-8 text-2xl items-center ms-auto">
