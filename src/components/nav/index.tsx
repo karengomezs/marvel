128adad202816ab7b67790dcbd8c0ff1b46f6b6a
karengomezs <karengomezs0601@gmail.com>
2023-05-23 21:38:11 -0500

    dialog ok

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -8,12 +8,12 @@ export default function Nav() {
 
   return (
     <>
-      <nav className="bg-dark-1 text-gold-1 h-[116px] flex flex-col sm:flex-row px-16 border-b-1 border-gris-3">
+      <nav className="bg-dark-1 text-gold-1 h-[116px] flex md:px-16 border-b-1 border-gris-3">
         {/* <div className="flex flex-col"> */}
-        <img src="/logo.svg" className="" alt="" />
+        <img src="/logo.svg" className="h-full" alt="" />
 
-        <ul className="flex ">
-          <li className="w-[114px] flex items-center justify-center group">
+        <ul className="md:flex hidden">
+          <li className="w-[114px] flex items-center justify-center group ">
             <img
               className="absolute bottom-0 hidden group-hover:block"
               src="/vector.png"
@@ -37,7 +37,7 @@ export default function Nav() {
         </ul>
         {/* </div> */}
 
-        <div className="flex gap-8 text-2xl items-center ms-auto">
+        <div className="md:flex gap-8 text-2xl items-center ms-auto hidden">
           <i className="fa-solid fa-bell text-white hover:text-gold-2"></i>
           <i className="fa-solid fa-gear text-white hover:text-gold-2"></i>
           <Link href="https://github.com/karengomezs/marvel" target="_blank">
@@ -46,31 +46,56 @@ export default function Nav() {
           <Link href="https://karengomez.netlify.app/" target="_blank">
             <i className="fa-solid fa-user hover:text-gold-2"></i>
           </Link>
+        </div>
+        <div className="md:hidden my-auto ml-auto mr-8">
+          <i className="fa-solid fa-bell text-white hover:text-gold-2"></i>
           <i
             onClick={() => {
               setModal(!modal);
             }}
-            className="fa-solid fa-bars sm:hidden text-2xl text-orange-600"
+            className="fa-solid fa-bars text-xl ml-4"
           ></i>
         </div>
       </nav>
 
-      <dialog open={modal} className="w-full h-full fixed top-0 bottom-0 p-0">
+      <dialog
+        open={modal}
+        className="z-10 w-full h-full fixed top-0 bottom-0 p-0 bg-gris-4 text-gold-2"
+      >
         <div className="flex justify-between bg-orange-600 py-8 px-4 items-center text-white">
           <i
             onClick={() => {
               setModal(!modal);
             }}
-            className="fa-solid fa-x"
+            className="fa-solid fa-x text-xl"
           ></i>
-          <p className="text-3xl">Menú</p>
+          <p className="text-2xl">Menú</p>
         </div>
-        <ul className="text-stone-600 mt-5 flex flex-col gap-5 ml-4 text-xl">
+        <ul className="text-stone-600 mt-5 flex flex-col items-end mr-4 gap-5 text-xl">
           <li>
-            <Link href="/">Home</Link>
+            <span>Home</span> <i className="fa-solid fa-house ml-2"></i>
           </li>
-          <li>Nosotros</li>
-          <li>Videos</li>
+          <li>
+            <span>Characters</span> <i className="fa-solid fa-users ml-2"></i>
+          </li>
+
+          <li>
+            <span>Settings </span>
+            <i className="fa-solid fa-gear text-white  ml-2"></i>
+          </li>
+          <li>
+            <Link href="https://github.com/karengomezs/marvel" target="_blank">
+              <span>Repo </span>
+
+              <i className="fa-brands fa-github ml-2"></i>
+            </Link>
+          </li>
+          <li>
+            <Link href="https://karengomez.netlify.app/" target="_blank">
+              <span>Portfolio </span>
+              <i className="fa-solid fa-user  ml-2"></i>
+            </Link>
+          </li>
         </ul>
       </dialog>
     </>
